a9daf471f1c75dc7765313d0af3b847ebc098d24
adonisbovell <yoshimitsu12@gmail.com>
2018-11-05 20:31:13 -0500

    Final newline

--- init.el
+++ init.el
@@ -150,3 +150,21 @@
 
 ;; Go configuration
 (load "go.el")
+
+;; Load sane default
+(setq require-final-newline t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(package-selected-packages
+   (quote
+    (elpy use-package tagedit smex rainbow-delimiters projectile paredit markdown-mode magit ido-ubiquitous go-mode go-autocomplete exec-path-from-shell clojure-mode-extra-font-locking cider better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
