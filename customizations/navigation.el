2c2149c9ad87ac11ae43164602a1229e467c61a0
Adonis Bovell <recursiveMake@users.noreply.github.com>
2019-04-23 12:09:09 -0400

    Add projectile keybindings
    New versions of projectile do not add default keybindings

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -59,3 +59,9 @@
 
 ;; projectile everywhere!
 (projectile-global-mode)
+
+;; projectile bindings
+(require 'projectile)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
