c4adb14aaaf71fdebc1f4427021cac69072190f4
adonisbovell <recursiveMake@users.noreply.github.com>
2019-05-26 22:29:34 -0400

    Remove marmalade
    Runs with an expired certificate

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 
 ;; Define package repositories
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
