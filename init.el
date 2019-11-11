2d5d8929c819c9efb06d5500f5aac0de56a79438
Adonis Bovell <recursiveMake@users.noreply.github.com>
2019-11-11 15:50:56 -0500

    Auto-guess LSP root to avoid annoying messages

--- init.el
+++ init.el
@@ -180,6 +180,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
+ '(lsp-auto-guess-root t)
  '(package-selected-packages
    (quote
     (
