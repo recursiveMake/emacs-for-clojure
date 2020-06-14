7fb9842890cb06972de2f45c056f4b39d4ca213c
adonisbovell <recursiveMake@users.noreply.github.com>
2020-06-14 01:59:10 -0400

    Disable unused package repos

--- init.el
+++ init.el
@@ -4,10 +4,6 @@
 
 ;; Define package repositories
 (require 'package)
-;; (add-to-list 'package-archives
-;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
