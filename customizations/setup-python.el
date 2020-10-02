7b54d6c147015e42c90094e03ddb9be98e46ab4a
Adonis Bovell <recursiveMake@users.noreply.github.com>
2020-10-02 13:43:38 -0400

    Disable flymake
    This is turned on in the major mode

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -3,8 +3,15 @@
   :init
   (elpy-enable))
 
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
 
 ;; Use Python 3
 (setq elpy-rpc-python-command "~/Documents/workspace/.envs/emacs-python-3/bin/python")
 
 (setq python-shell-interpreter "python3")
+
+(add-hook 'python-mode-hook (lambda ()
+                              (flymake-mode -1)))
