2a442e0e3a18f022bb71fd65cd41c1b6fb6a9561
Adonis Bovell <recursiveMake@users.noreply.github.com>
2020-10-02 13:48:00 -0400

    Kill magit windows

--- customizations/git.el
+++ customizations/git.el
@@ -1,4 +1,14 @@
+(require 'magit)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (global-diff-hl-mode)
+
+(defun mu-magit-kill-buffers ()
+  "Restore window configuration and kill all Magit buffers."
+  (interactive)
+  (let ((buffers (magit-mode-get-buffers)))
+    (magit-restore-window-configuration)
+    (mapc #'kill-buffer buffers)))
+
+(bind-key "q" #'mu-magit-kill-buffers magit-status-mode-map)
