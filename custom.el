4198750b4c51bf4791024eb7d5d120b21b50ec83
Adonis Bovell <recursiveMake@users.noreply.github.com>
2026-03-21 08:40:03 -0400

    Select python for emacs

--- custom.el
+++ custom.el
@@ -5,50 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-django elpy-module-sane-defaults)))
+   '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-django elpy-module-sane-defaults))
+ '(elpy-rpc-python-command "python3")
  '(lsp-auto-guess-root t)
  '(package-selected-packages
-   (quote
-    (
-     ag
-     yasnippet-snippets
-     direnv
-     all-the-icons
-     better-defaults
-     cider
-     clojure-mode-extra-font-locking
-     diff-hl
-     dockerfile-mode
-     doom-modeline
-     ein
-     elpy
-     ess
-     exec-path-from-shell
-     flycheck
-     go-autocomplete
-     go-mode
-     groovy-mode
-     lsp-mode
-     lsp-ui
-     magit
-     markdown-mode
-     org
-     paredit
-     projectile
-     py-autopep8
-     rainbow-delimiters
-     salt-mode
-     smex
-     tagedit
-     use-package
-     virtualenvwrapper
-     yasnippet
-     )))
- '(safe-local-variable-values
-   (quote
-    ((my-black-fix-code-on-save . t)
-     (my-black-fix-code . t)))))
+   '(ag yasnippet-snippets direnv all-the-icons better-defaults cider clojure-mode-extra-font-locking diff-hl dockerfile-mode doom-modeline ein elpy ess exec-path-from-shell flycheck go-autocomplete go-mode groovy-mode lsp-mode lsp-ui magit markdown-mode org paredit projectile py-autopep8 rainbow-delimiters salt-mode smex tagedit use-package virtualenvwrapper yasnippet))
+ '(python-shell-interpreter
+   "/Users/adonisbovell/Local/workspace/.envs/emacs-python-3/bin/python")
+ '(safe-local-variable-values '((my-black-fix-code-on-save . t) (my-black-fix-code . t))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
